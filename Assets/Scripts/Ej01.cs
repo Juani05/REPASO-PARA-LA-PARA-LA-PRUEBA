0f486f60f375f6a49dc31d52ff64fd7e300a7a76
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-04-07 08:48:54 -0300

    finshed exercises 1 and 2

---
+++ Assets/Scripts/Ej01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej01 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+        if (num1==num2)
+        {
+            Debug.Log("Los números son iguales");
+        }
+        else
+        {
+            Debug.Log("Los números son distintos");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej02.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej02 : MonoBehaviour
+{
+    public float precioproducto1;
+    public float precioproducto2;
+    public float precioproducto3;
+    public float montodisponible;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 2.Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+        float resta;
+        float sumadeproductos;
+        sumadeproductos = precioproducto1 + precioproducto2 + precioproducto3;
+        resta = montodisponible - sumadeproductos; 
+        if (sumadeproductos < montodisponible)
+        {
+            Debug.Log("La suma de productos no supera el monto disponible");
+            Debug.Log("A usted le quedan $" + resta + " disponibles");
+        }
+        else
+        {
+            Debug.Log("La suma de productos supera el monto disponible");
+            Debug.Log("Le faltan $" + resta*-1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
